6f5e76a647f7a1c1e7fd83f75ae4301c75d7d507
배인성 <duddl6701@naver.com>
2022-12-31 20:22:35 +0900

    2022.12.31 git test file 삭제

--- src/main/java/my/sideproject/ownus/controller/GitTest.java
+++
@@ -1,7 +0,0 @@
-package my.sideproject.ownus.controller;
-
-public class GitTest {
-    public void test(){
-        String s = "git 커밋 성공";
-    }
-}
--- src/main/java/my/sideproject/ownus/controller/GitTestTest.java
+++
@@ -1,4 +0,0 @@
-package my.sideproject.ownus.controller;
-
-public class GitTestTest {
-}
