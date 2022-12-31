3221db1c72c96c89ed1c4012ebc0a00190e2b56d
배인성 <duddl6701@naver.com>
2022-12-31 17:22:20 +0900

    2022.12.31 프로젝트 생성

---
+++ src/main/java/my/sideproject/ownus/OwnusApplication.java
@@ -0,0 +1,13 @@
+package my.sideproject.ownus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/sideproject/ownus/OwnusApplicationTests.java
@@ -0,0 +1,13 @@
+package my.sideproject.ownus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
